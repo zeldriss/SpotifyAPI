3a149acfcb6d19cefe5d03b7857b229b7f1aac4a
zeldriss <31651756+zeldriss@users.noreply.github.com>
2020-04-26 04:19:38 +0700

    Create example.php

---
+++ example.php
@@ -0,0 +1,19 @@
+<?php
+
+require "function.php";
+
+$spotify = new spotify();
+
+
+$username = "";
+$password = "";
+$login = $spotify->tryLogin($username, $password);
+$token = $spotify->getToken($login[1]);
+
+$json = json_decode($token[1], true);
+
+$token = $json['accessToken'];
+
+print_r($spotify->followUser($token, "zeldriss"));
+print_r($spotify->isFollowUser($token, "zeldriss"));
+?>
